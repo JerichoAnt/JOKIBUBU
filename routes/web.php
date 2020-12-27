7977699fc8e6b5e81dd1c1788e00bffd093f513e
Tanjiro04 <tanjiro04@users.noreply.github.com>
2020-12-27 17:02:30 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('layout.adminlte');
+// });
+
 Route::get('/', function () {
     return view('welcome');
 });
